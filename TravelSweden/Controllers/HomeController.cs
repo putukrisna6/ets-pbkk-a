29582c7bce54b93f2d5922318ffa8101989e0dd6
vincentyonathan <vincentyonathan24@gmail.com>
2022-04-08 23:54:40 +0700

    feat : another booking page (book)

--- TravelSweden/Controllers/HomeController.cs
+++ TravelSweden/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace TravelSweden.Controllers
             return View();
         }
 
+        public IActionResult Book()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
