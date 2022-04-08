1ea432ac4b5b66a59d4bc290170b361b081ec0be
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-04-08 21:49:22 +0700

    feat: Airport and FlightRoute model

---
+++ TravelSweden/Models/Airport.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelSweden.Models
+{
+    public class Airport
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Iata { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+
+        public Airport()
+        {
+
+        }
+    }
+}
