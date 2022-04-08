bcee50cd07b8f8c8a3b77be05fd0c2c5609b4221
vincentyonathan <vincentyonathan24@gmail.com>
2022-04-09 00:27:58 +0700

    feat : new page payment done

--- TravelSweden/Controllers/HomeController.cs
+++ TravelSweden/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace TravelSweden.Controllers
             return View();
         }
 
+        public IActionResult Payment()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
