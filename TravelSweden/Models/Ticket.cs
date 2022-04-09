aede07e749b11f5f0ea0ffbc5ac5591296d7a9c2
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2022-04-09 10:19:54 +0700

    feat: modify Ticket and add AvailableTicket

--- TravelSweden/Models/Ticket.cs
+++ TravelSweden/Models/Ticket.cs
@@ -7,12 +7,8 @@ namespace TravelSweden.Models
         [Required]
         public int Id { get; set; }
         [Required]
-        public int BookedFlightId { get; set; }
-        public Flight BookedFlight { get; set; }
-        [Required]
-        public string Category { get; set; }
-        [Required]
-        public double Price { get; set; }
+        public int BookedTicketId { get; set; }
+        public AvailableTicket BookedTicket { get; set; }
         public string PassengerName { get; set; }
         public string PassengerId { get; set; }
         public string PassengerAddress { get; set; }
